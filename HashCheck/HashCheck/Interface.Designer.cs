74a41e30c93736fb235e20b30c4fecb29ff4350d
Aaron R. Muir <aaronmuir@gmail.com>
2014-12-09 23:19:20 -0600

    added checkboxes to select encryption

--- HashCheck/HashCheck/Interface.Designer.cs
+++ HashCheck/HashCheck/Interface.Designer.cs
@@ -35,8 +35,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.hashTable = new System.Windows.Forms.TableLayoutPanel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.chkMD5 = new System.Windows.Forms.CheckBox();
+            this.chkSHA256 = new System.Windows.Forms.CheckBox();
             this.lblSHA256 = new System.Windows.Forms.Label();
             this.lblMD5 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
@@ -52,50 +52,56 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 10);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.07583F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.92417F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(518, 191);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(636, 149);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.groupBox2);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 8);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(502, 73);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(624, 58);
             this.flowLayoutPanel1.TabIndex = 3;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.lblFileName);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Location = new System.Drawing.Point(2, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(502, 64);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(622, 52);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "File";
             // 
             // lblFileName
             // 
-            this.lblFileName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblFileName.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblFileName.AutoSize = true;
-            this.lblFileName.Location = new System.Drawing.Point(119, 28);
+            this.lblFileName.Location = new System.Drawing.Point(99, 23);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(126, 17);
+            this.lblFileName.Size = new System.Drawing.Size(95, 13);
             this.lblFileName.TabIndex = 3;
             this.lblFileName.Text = "SomeFileName.jpg";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Location = new System.Drawing.Point(4, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 31);
+            this.button1.Size = new System.Drawing.Size(80, 25);
             this.button1.TabIndex = 2;
             this.button1.Text = "Select File";
             this.button1.UseVisualStyleBackColor = true;
@@ -104,9 +110,11 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.hashTable);
-            this.groupBox1.Location = new System.Drawing.Point(8, 87);
+            this.groupBox1.Location = new System.Drawing.Point(6, 68);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(502, 86);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(624, 75);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Hashes";
@@ -114,68 +122,83 @@
             // hashTable
             // 
             this.hashTable.ColumnCount = 2;
-            this.hashTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.1129F));
-            this.hashTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.8871F));
-            this.hashTable.Controls.Add(this.label2, 0, 1);
-            this.hashTable.Controls.Add(this.label1, 0, 0);
+            this.hashTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.78317F));
+            this.hashTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.21683F));
+            this.hashTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 260F));
+            this.hashTable.Controls.Add(this.chkMD5, 0, 1);
+            this.hashTable.Controls.Add(this.chkSHA256, 0, 0);
             this.hashTable.Controls.Add(this.lblSHA256, 1, 0);
             this.hashTable.Controls.Add(this.lblMD5, 1, 1);
-            this.hashTable.Location = new System.Drawing.Point(3, 18);
+            this.hashTable.Location = new System.Drawing.Point(2, 15);
+            this.hashTable.Margin = new System.Windows.Forms.Padding(2);
             this.hashTable.Name = "hashTable";
             this.hashTable.RowCount = 2;
             this.hashTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.hashTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.hashTable.Size = new System.Drawing.Size(496, 60);
+            this.hashTable.Size = new System.Drawing.Size(618, 49);
             this.hashTable.TabIndex = 0;
             // 
-            // label2
+            // chkMD5
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "MD5";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkMD5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkMD5.AutoSize = true;
+            this.chkMD5.Checked = true;
+            this.chkMD5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMD5.Location = new System.Drawing.Point(3, 27);
+            this.chkMD5.Name = "chkMD5";
+            this.chkMD5.Size = new System.Drawing.Size(73, 19);
+            this.chkMD5.TabIndex = 9;
+            this.chkMD5.Text = "MD5";
+            this.chkMD5.UseVisualStyleBackColor = true;
+            this.chkMD5.CheckedChanged += new System.EventHandler(this.chkMD5_CheckedChanged);
             // 
-            // label1
+            // chkSHA256
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "SHA256";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkSHA256.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkSHA256.AutoSize = true;
+            this.chkSHA256.Checked = true;
+            this.chkSHA256.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSHA256.Location = new System.Drawing.Point(3, 3);
+            this.chkSHA256.Name = "chkSHA256";
+            this.chkSHA256.Size = new System.Drawing.Size(73, 18);
+            this.chkSHA256.TabIndex = 8;
+            this.chkSHA256.Text = "SHA256";
+            this.chkSHA256.UseVisualStyleBackColor = true;
+            this.chkSHA256.CheckedChanged += new System.EventHandler(this.chkSHA256_CheckedChanged);
             // 
             // lblSHA256
             // 
             this.lblSHA256.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblSHA256.AutoSize = true;
-            this.lblSHA256.Location = new System.Drawing.Point(72, 6);
+            this.lblSHA256.Location = new System.Drawing.Point(81, 5);
+            this.lblSHA256.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSHA256.Name = "lblSHA256";
-            this.lblSHA256.Size = new System.Drawing.Size(126, 17);
+            this.lblSHA256.Size = new System.Drawing.Size(49, 13);
             this.lblSHA256.TabIndex = 6;
-            this.lblSHA256.Text = "SomeFileName.jpg";
+            this.lblSHA256.Text = "<HASH>";
             // 
             // lblMD5
             // 
             this.lblMD5.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblMD5.AutoSize = true;
-            this.lblMD5.Location = new System.Drawing.Point(72, 36);
+            this.lblMD5.Location = new System.Drawing.Point(81, 30);
+            this.lblMD5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMD5.Name = "lblMD5";
-            this.lblMD5.Size = new System.Drawing.Size(126, 17);
+            this.lblMD5.Size = new System.Drawing.Size(49, 13);
             this.lblMD5.TabIndex = 7;
-            this.lblMD5.Text = "SomeFileName.jpg";
+            this.lblMD5.Text = "<HASH>";
             // 
             // Interface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 214);
+            this.ClientSize = new System.Drawing.Size(656, 170);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Interface";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Interface_Load);
@@ -199,9 +222,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TableLayoutPanel hashTable;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblSHA256;
         private System.Windows.Forms.Label lblMD5;
+        private System.Windows.Forms.CheckBox chkMD5;
+        private System.Windows.Forms.CheckBox chkSHA256;
     }
 }
--- HashCheck/HashCheck/Interface.cs
+++ HashCheck/HashCheck/Interface.cs
@@ -12,6 +12,9 @@ namespace HashCheck
 {
     public partial class Interface : Form
     {
+        private Boolean sha256Checked = true;
+        private Boolean md5Checked = true;
+
         public Interface()
         {
             InitializeComponent();
@@ -21,17 +24,48 @@ namespace HashCheck
         {
             HashCheck.selectFiles();
 
-            foreach(HashCheck.file f in HashCheck.hashCollection)
+            if(md5Checked)
+                HashCheck.computeHash(HashCheck.hashAlgorithm.MD5);
+
+            if (sha256Checked)
+                HashCheck.computeHash(HashCheck.hashAlgorithm.SHA256);
+
+            if (HashCheck.filesToCheck != null)
             {
-                lblFileName.Text = f.fileName;
-                lblSHA256.Text = f.hashes[(int)HashCheck.encryption.SHA256].hashString;
-                lblMD5.Text = f.hashes[(int)HashCheck.encryption.MD5].hashString;
+                foreach (HashFile f in HashCheck.filesToCheck)
+                {
+                    lblFileName.Text = f.getFileName();
+
+                    if (sha256Checked)
+                        lblSHA256.Text = f.getHashes()[(int)HashCheck.hashAlgorithm.SHA256].hashString;
+
+                    if (md5Checked)
+                        lblMD5.Text = f.getHashes()[(int)HashCheck.hashAlgorithm.MD5].hashString;
+                }
             }
         }
 
         private void Interface_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void chkSHA256_CheckedChanged(object sender, EventArgs e)
+        {
+            sha256Checked = chkSHA256.Checked;
+        }
+
+        private void chkMD5_CheckedChanged(object sender, EventArgs e)
+        {
+            md5Checked = chkMD5.Checked;
+        }
+        public bool getSHA256Checked()
+        {
+            return sha256Checked;
+        }
+        public bool getMD5Checked()
+        {
+            return md5Checked;
         }
     }
 }
